3134209373ba7474b4bd75084c60ab5b4db4bdab
loic.gardaire <LoicAlbertChirstian.GARDAIRE@de.amadeus.com>
2018-07-03 10:38:35 +0200

    correct html for w3c + cached files

---
+++ sw.js
@@ -0,0 +1,27 @@
+const cacheName = `my_first_pwa`;
+self.addEventListener('install', e => {
+    const timeStamp = Date.now();
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll([
+                `/`,
+                `/index.html`
+            ])
+                .then(() => self.skipWaiting());
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.open(cacheName)
+            .then(cache => cache.match(event.request, {ignoreSearch: true}))
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});

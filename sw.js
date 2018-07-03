07c961f2c8fe1d6f4f98e1788782b05e2623c574
loic.gardaire <LoicAlbertChirstian.GARDAIRE@de.amadeus.com>
2018-07-03 16:14:25 +0200

    pass get parameter in start url

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ self.addEventListener('install', e => {
             return cache.addAll([
                 `/`,
                 `/index.html`,
-                "/images/Amadeus_Logo.png",
+                "/images/Amadeus_Logo.png"
             ])
                 .then(() => self.skipWaiting());
         })

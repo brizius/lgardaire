4e5c9dc90c44d9344ea8bce413b3c718c9ef059d
loic.gardaire <LoicAlbertChirstian.GARDAIRE@de.amadeus.com>
2018-07-04 17:06:24 +0200

    cache the icon to install the pwa

--- sw.js
+++ sw.js
@@ -6,7 +6,8 @@ self.addEventListener('install', e => {
             return cache.addAll([
                 "./",
                 `./index.html`,
-                "./images/Amadeus_Logo.png"
+                "./images/Amadeus_Logo.png",
+                "./images/Amadeus_icon144.png"
             ])
                 .then(() => self.skipWaiting());
         })

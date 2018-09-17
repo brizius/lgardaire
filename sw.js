6408fb34a1dac0c210a9e0895dee3fc21d1e0373
fellena <fabrizio.ellena@amadeus.com>
2018-09-17 19:16:58 +0200

    add redirection on click

--- sw.js
+++ sw.js
@@ -50,7 +50,26 @@ self.addEventListener('push', event => {
     event.waitUntil(self.registration.showNotification(title, options));
 });
 
+
 self.addEventListener('notificationclick', function(event) {
     console.log('[Service Worker] Notification click Received.');
-    event.notification.close();
+
+    let url = 'https://www.instagram.com/explore/tags/MyStopover/';
+    event.notification.close(); // Android needs explicit close.
+    event.waitUntil(
+        clients.matchAll({type: 'window'}).then( windowClients => {
+            // Check if there is already a window/tab open with the target URL
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                // If so, just focus it.
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            // If not, then open the target URL in a new window/tab.
+            if (clients.openWindow) {
+                return clients.openWindow(url);
+            }
+        })
+    );
 });
